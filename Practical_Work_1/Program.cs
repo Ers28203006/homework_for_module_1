fec863d26ec7590e4dfff7299195f0f13c06540f
ersyn_sultanbaev <ersyn_sultanbaev@mail.ru>
2018-12-22 23:59:40 +0600

    Добавьте файлы проекта.

---
+++ Practical_Work_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Work_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*------- Написать приложение, которое выведет на экран Ваше имя и фамилию -------*/
+
+            Console.Write("Меня зовут ");
+            Console.WriteLine("Ерсын Султанбаев");
+
+            /*------- Написать приложение, которое ожидает ввода нескольких чисел и выводит на экран их сумму -------*/
+
+            int n = 5, number, summa=0;
+            for (int i=0; i<n; i++)
+            {
+                Console.Write("Введите число: ");
+                number = int.Parse(Console.ReadLine());
+                summa += number;
+            }
+
+            Console.Write("Cумма введенных чисел: ");
+            Console.WriteLine(summa);
+
+            Console.ReadLine();
+        }
+    }
+}
